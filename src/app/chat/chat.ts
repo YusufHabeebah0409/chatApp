1fb5d3e27dfa65e9bbdefe2dd1da37dea74af4a5
Habeebah <ojuolapehabeebah@gmail.com>
2025-10-30 22:25:33 +0000

    Add Chat component with routing and initial setup
    - Created Chat component with associated HTML, CSS, and spec files.
    - Updated routing to include Chat component as the default route.

---
+++ src/app/chat/chat.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  imports: [],
+  templateUrl: './chat.html',
+  styleUrl: './chat.css',
+})
+export class Chat implements OnInit {
+  userName:string = '';
+  
+  ngOnInit(): void {
+   
+  }
+
+
+}
