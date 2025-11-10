f066fde52d368a288410d7bfa0618a57239982f3
Habeebah <ojuolapehabeebah@gmail.com>
2025-11-10 17:59:12 +0000

    Implement chat component with user input and message sending functionality

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [CommonModule, FormsModule,HttpClientModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
 export class Chat implements OnInit {
+  private _http = inject(HttpClient);
   userName:string = '';
+  messageText:string = '';
   
   ngOnInit(): void {
-   
+    this.userName = prompt("Enter Your Name ") || "Guest";
   }
+
+ sendChat(): void{
+  const newData = {
+    username: this.userName,
+    message: this.messageText
+  };
+  this._http.post('http://localhost/my-project-php/chatApp/sendMessage.php', newData).subscribe({
+    next: (response) => {
+    console.log('Message sent successfully', response);
+    this.messageText = '';
+    },
+    error: (error) => console.error('Error sending message', error)
+  })
+}
+
+
 
 
 }
