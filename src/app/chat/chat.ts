67e290cf226d9f934b6956608f9a315a03ed2319
Habeebah <ojuolapehabeebah@gmail.com>
2025-11-16 00:04:37 +0000

    Enhance chat component with styling, message display, and retrieval functionality

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -12,28 +12,41 @@ import { FormsModule } from '@angular/forms';
 })
 export class Chat implements OnInit {
   private _http = inject(HttpClient);
-  userName:string = '';
-  messageText:string = '';
+  userName= '';
+  messageText = '';
+  messages: any[] = [];
   
+
   ngOnInit(): void {
     this.userName = prompt("Enter Your Name ") || "Guest";
+    setInterval(() => {
+      this.getChat();
+    }, 2000);
   }
 
- sendChat(): void{
-  const newData = {
-    username: this.userName,
-    message: this.messageText
-  };
-  this._http.post('http://localhost/my-project-php/chatApp/sendMessage.php', newData).subscribe({
-    next: (response) => {
-    console.log('Message sent successfully', response);
-    this.messageText = '';
-    },
-    error: (error) => console.error('Error sending message', error)
-  })
-}
 
+  sendChat(): void {
+    const newData = {
+      username: this.userName,
+      message: this.messageText
+    };
+    this._http.post('http://localhost/my-project-php/chatApp/sendMessage.php', newData).subscribe({
+      next: (response) => {
+        console.log('Message sent successfully', response);
+        this.messageText = '';
+      },
+      error: (error) => console.error('Error sending message', error)
+    })
+  }
 
+getChat(): void {
+    this._http.get<any[]>('http://localhost/my-project-php/chatApp/getMessage.php').subscribe({
+      next: (data) => {
+        this.messages = data;
+      },
+      error: (error) => console.error('Error retrieving messages', error)
+    });
+  }
 
 
 }
